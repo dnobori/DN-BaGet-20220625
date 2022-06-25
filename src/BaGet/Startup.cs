4bb231840f306af9135da95c9801d89ec1deee2a
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2022-06-25 16:46:06 +0900

    Allow reverse proxy

--- src/BaGet/Startup.cs
+++ src/BaGet/Startup.cs
@@ -94,6 +94,8 @@ namespace BaGet
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseForwardedHeaders();
+
             var options = Configuration.Get<BaGetOptions>();
 
             if (env.IsDevelopment())
